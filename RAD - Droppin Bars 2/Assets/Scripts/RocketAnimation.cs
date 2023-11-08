a16630501951c7ce0124f28400a578b329f82d6c
AnthonyTomas2011 <130806226+MinnieMia@users.noreply.github.com>
2023-11-07 18:29:22 -0600

    Step 48

--- RAD - Droppin Bars 2/Assets/Scripts/RocketAnimation.cs
+++ RAD - Droppin Bars 2/Assets/Scripts/RocketAnimation.cs
@@ -2,17 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class RocketAnimation : MonoBehaviour
 {
+     [Header("Animator")]
+     public Animator animator;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        float horizontal = Input.GetAxis("Horizontal");
+
+        animator.SetFloat("HAxis", horizontal);
     }
 }
