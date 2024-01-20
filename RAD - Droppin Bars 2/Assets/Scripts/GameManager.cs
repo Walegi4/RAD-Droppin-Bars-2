e8a4f481894b9b3503f24bad82bf34651447f68d
AnthonyTomas2011 <ramoscalixj@gmail.com>
2024-01-19 19:03:05 -0600

    Droppin Bars Pt.3, 25

---
+++ RAD - Droppin Bars 2/Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    privateSpawner spawner;
+
+    void Awake()
+    {
+        spawner = GameObject.Find("Spawner").GetComponent<Spawner>();
+    }
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawner.active = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.anyKeyDown)
+        {
+            spawner.active = true;
+        }
+    }
+}
